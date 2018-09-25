0a81b96f092de62d5cee59c43aab2bc3a5dbebca
Evgeny Khramtsov <ekhramtsov@process-one.net>
2018-09-26 01:24:11 +0300

    Add CA bundle updating script

---
+++ tools/update-ca.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+##################################################################
+#
+#  Usage:
+#  - clone the repo:
+#    $ git clone git@github.com:processone/pkix.git
+#  - run the script:
+#    $ pkix/tools/update-ca.sh
+#
+#  NOTES; You absolutely need to run the script from cloned repo
+#         and have write access to its remote
+#
+##################################################################
+
+SCRIPT_DIR=$(dirname -z $0)
+GIT_ROOT=$(git -C $SCRIPT_DIR rev-parse --show-toplevel)
+CAFILE=$GIT_ROOT/priv/cacert.pem
+
+curl --time-cond $CAFILE --output $CAFILE https://curl.haxx.se/ca/cacert.pem
+CHANGES=$(git -C $GIT_ROOT diff --name-only $CAFILE)
+if [ ! -z "$CHANGES" ]; then
+    git -C $GIT_ROOT add $CAFILE
+    git -C $GIT_ROOT commit -m "Update CA bundle"
+    git -C $GIT_ROOT push
+fi
